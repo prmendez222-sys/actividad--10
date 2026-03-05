cf7590e44360e2143a424e671ecf8625a023d775
pedro mendez <prmendez222@gmail.com>
2026-03-05 11:20:52 -0600

    final ejercicio 1

--- ejercicico 1/Program.cs
+++ ejercicico 1/Program.cs
@@ -5,8 +5,9 @@ string opcion;
 do
 {
     Console.WriteLine("1. ingresar estudiante");
-    Console.WriteLine("2. buscar estudiante");
-    Console.WriteLine("3.salir");
+    Console.WriteLine("2. mostrar todos lo estudiantes");
+    Console.WriteLine("3. buscar estudiante");
+    Console.WriteLine("4. salir");
     Console.WriteLine();
     Console.Write("ingrese una opcion: ");
 
@@ -32,6 +33,52 @@ do
             Console.WriteLine("Presione enter para finalizar");
             Console.ReadLine();
             break;
+        case "2":
+            Console.WriteLine("ID        estudiante");
+            Console.WriteLine();
+            foreach(var n in estrudiante)
+            {
+                Console.WriteLine($"{n.Key}         {n.Value}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Presione enter para finalizar");
+            Console.ReadLine();
+            break;
+        case "3":
+            Console.Write("ingrese ID del estudiante: ");
+            int ID=int.Parse(Console.ReadLine());
+
+            if (estrudiante.ContainsKey(ID))
+            {
+                Console.WriteLine($"estudiante encontrado: {estrudiante[ID]}");
+            }
+            else
+            {
+                Console.WriteLine("numero de ID no existente");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Presione enter para finalizar");
+            Console.ReadLine();
+            break;
+        case "4":
+            Console.Write("saliendo");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            break;
+        default:
+            Console.WriteLine("opcion no valida");
+            Console.WriteLine();
+            Console.WriteLine("Presione enter para finalizar");
+            Console.ReadLine();
+            break;
     }
     Console.Clear();
-} while (opcion!="3");
+} while (opcion!="4");
