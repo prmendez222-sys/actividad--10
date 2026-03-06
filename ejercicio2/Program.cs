c67c88d9db6938c1173e6d11a3875820dfc63c33
pedro mendez <prmendez222@gmail.com>
2026-03-05 23:41:55 -0600

    ejercicio 2 completo

--- ejercicio2/Program.cs
+++ ejercicio2/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<int,string> prodructos = new Dictionary<int,string>();
+string opcion;
+int contP = 0;
+do
+{
+    Console.WriteLine("1. ingresar Producto");
+    Console.WriteLine("2. ver lista de productos");
+    Console.WriteLine("3. salir");
+    Console.WriteLine();
+    Console.Write("ingrese una opcion: ");
+    opcion=Console.ReadLine();
+    Console.Clear();
+
+    switch (opcion)
+    {
+        case "1":
+            Console.Write("ingrese nombre del Producto: ");
+            prodructos.Add(contP,Console.ReadLine());
+            contP++;
+            Console.WriteLine();
+            Console.WriteLine("Producto ingresado con exito");
+            Console.WriteLine();
+            Console.WriteLine("Presione Enter Para continuar");
+            Console.ReadLine();
+            break;
+        case "2":
+            if (prodructos.Count == 0)
+            {
+                Console.WriteLine("no hay productos ingresados");
+            }
+            else
+            {
+                Console.WriteLine("ID         nombre del Producto");
+                foreach (var p in prodructos)
+                {
+                    Console.WriteLine($"{p.Key}          {p.Value}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Presione Enter Para continuar");
+            Console.ReadLine();
+            break;
+        case "3":
+            Console.WriteLine("saliendo...");
+            Thread.Sleep(2000);
+            break;
+        default:
+            Console.WriteLine("opcion no valida");
+            Console.WriteLine();
+            Console.WriteLine("Presione Enter Para continuar");
+            Console.ReadLine();
+            break;
+    }
+
+    Console.Clear();
+
+} while (opcion != "3");
