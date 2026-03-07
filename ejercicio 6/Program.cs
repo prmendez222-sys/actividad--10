865f463e9b89c9f26e7613163931a3b037f0c4cd
pedro mendez <prmendez222@gmail.com>
2026-03-06 23:19:04 -0600

    ejercicio 7 terminado

---
+++ ejercicio 6/Program.cs
@@ -0,0 +1,67 @@
+﻿void Presionar()
+{
+    Console.WriteLine();
+    Console.WriteLine("Presione Enter Para continuar");
+    Console.ReadLine();
+}
+
+Dictionary<int,string> estudiantes = new Dictionary<int,string>();
+string opcion;
+
+for (int i = 0; i < 3; i++)
+{
+    Console.Write($"ingrese nombre del estudiante {i+1}: ");
+    estudiantes.Add(i, Console.ReadLine());
+    Console.WriteLine();
+}
+
+Console.WriteLine("estudiantes registrados correctamente");
+Presionar();
+Console.Clear();
+
+do
+{
+    Console.WriteLine("1. ver estudiantes");
+    Console.WriteLine("2. eliminar estudiantes por ID");
+    Console.WriteLine("3. salir");
+    Console.WriteLine();
+    Console.Write("ingrese una opcion: ");
+    opcion= Console.ReadLine();
+    Console.Clear();
+    switch (opcion)
+    {
+        case "1":
+            Console.WriteLine("ID          Nombre estudiante");
+            Console.WriteLine();
+            foreach(var e in estudiantes)
+            {
+                Console.WriteLine($"{e.Key}          {e.Value}");
+            }
+            Presionar();
+            break;
+        case "2":
+            Console.Write("ingrese ID del estudiante: ");
+            int id=int.Parse(Console.ReadLine());
+
+            if (estudiantes.ContainsKey(id))
+            {
+                estudiantes.Remove(id);
+                Console.WriteLine("estudiante eliminado con exito");
+            }
+            else
+            {
+                Console.WriteLine("estudiante no existente");
+            }
+            Presionar();
+            break;
+        case "3":
+            Console.WriteLine("saliendo...");
+            Thread.Sleep(1000);
+            break;
+        default:
+            Console.WriteLine("opcion no valida");
+            Presionar();
+            break;
+    }
+    Console.Clear();
+} while (opcion!="3");
---
+++ ejercicio 7/Program.cs
@@ -0,0 +1,69 @@
+﻿void Presionar()
+{
+    Console.WriteLine();
+    Console.WriteLine("Presione Enter Para continuar");
+    Console.ReadLine();
+}
+Dictionary<string, string> prodructos = new Dictionary<string, string>();
+string opcion;
+
+do
+{
+    Console.WriteLine("1. ingresar Producto");
+    Console.WriteLine("2. ver lista de productos");
+    Console.WriteLine("3. salir");
+    Console.WriteLine();
+    Console.Write("ingrese una opcion: ");
+    opcion = Console.ReadLine();
+    Console.Clear();
+
+    switch (opcion)
+    {
+        case "1":
+            Console.Write("ingrese ID del Producto: ");
+            string idP= Console.ReadLine();
+            Console.WriteLine();
+
+            if (prodructos.ContainsKey(idP))
+            {
+                Console.WriteLine("el ID ya existe");
+            }
+            else
+            {
+                Console.Write("ingrese nombre del Producto: ");
+                string nombreP = Console.ReadLine();
+                Console.WriteLine();
+                prodructos.Add(idP, nombreP);
+                Console.WriteLine("Producto ingresado con exito");
+            }
+            Presionar();
+
+            break;
+        case "2":
+            if (prodructos.Count == 0)
+            {
+                Console.WriteLine("no hay productos ingresados");
+            }
+            else
+            {
+                Console.WriteLine("ID         nombre del Producto");
+                foreach (var p in prodructos)
+                {
+                    Console.WriteLine($"{p.Key}          {p.Value}");
+                }
+            }
+            Presionar();
+            break;
+        case "3":
+            Console.WriteLine("saliendo...");
+            Thread.Sleep(2000);
+            break;
+        default:
+            Console.WriteLine("opcion no valida");
+            Presionar();
+            break;
+    }
+
+    Console.Clear();
+
+} while (opcion != "3");
