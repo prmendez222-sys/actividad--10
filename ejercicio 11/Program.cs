6d43acbc8f0eaea4be2115f8a9d898c9cdfe8cbe
pedro mendez <prmendez222@gmail.com>
2026-03-07 00:32:11 -0600

    ejercicio 11 completo

---
+++ ejercicio 11/Program.cs
@@ -0,0 +1,22 @@
+﻿void Presionar()
+{
+    Console.WriteLine();
+    Console.WriteLine("Presione enter para continuar");
+    Console.ReadLine();
+}
+Dictionary<int, string> estudiantes = new Dictionary<int, string>();
+
+for(int i=0;  i<5; i++)
+{
+    Console.Write($"ingrese Nombre del estudiante {i+1}: ");
+    estudiantes.Add(i,Console.ReadLine());
+    Console.WriteLine();
+}
+Console.WriteLine("estudiantes ingresados con exito");
+Presionar();
+Console.Clear();
+Console.WriteLine("ID         Nombre estudiante");
+foreach(var e in estudiantes)
+{
+    Console.WriteLine($"{e.Key}         {e.Value}");
+}
