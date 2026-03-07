24225355e699f0fd8dd098c9951286397047684b
pedro mendez <prmendez222@gmail.com>
2026-03-06 23:55:32 -0600

    ejercicio 8 terminado

---
+++ ejercicio 8/Program.cs
@@ -0,0 +1,67 @@
+﻿void Presionar()
+{
+    Console.WriteLine();
+    Console.WriteLine("Presione Enter Para continuar");
+    Console.ReadLine();
+}
+
+Dictionary<int, string> empleados = new Dictionary<int, string>();
+string opcion;
+int contE = 0;
+do
+{
+    Console.WriteLine("1. ingrese un nuevo Empleado: ");
+    Console.WriteLine("2. ver empleados");
+    Console.WriteLine("3. modificar nombre de empleado");
+    Console.WriteLine("4. salir");
+    Console.WriteLine();
+    Console.Write("ingrese una opcion: ");
+    opcion= Console.ReadLine();
+    Console.Clear();
+    switch (opcion)
+    {
+        case "1":
+            Console.Write("ingrese nombre del empleado: ");
+            empleados.Add(contE, Console.ReadLine());
+            contE++;
+            Console.WriteLine("empleado registrado con exito");
+            Presionar();
+            break;
+        case "2":
+            Console.WriteLine("ID          Nombre de Empleado");
+            foreach(var e in empleados)
+            {
+                Console.WriteLine($"{e.Key}          {e.Value}");
+            }
+            Presionar();
+            break;
+        case "3":
+            Console.Write("ingrese ID del empleado: ");
+            int idE=int.Parse(Console.ReadLine());
+
+            if (empleados.ContainsKey(idE))
+            {
+                Console.Write("ingrese el nuevo nombre del empleado: ");
+                string nNombreE=Console.ReadLine();
+
+                empleados[idE] = nNombreE;
+
+                Console.WriteLine("nombre actualizado con exito");
+            }
+            else
+            {
+                Console.WriteLine("el Empleado no existe");
+            }
+            Presionar();
+            break;
+        case "4":
+            Console.WriteLine("saliendo...");
+            Thread.Sleep(1000);
+            break;
+        default:
+            Console.WriteLine("opcion no valida");
+            Presionar();
+            break;
+    }
+    Console.Clear();
+} while (opcion != "4");
