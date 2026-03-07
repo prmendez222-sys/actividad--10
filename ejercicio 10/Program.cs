8820f7ace204b023672951ea9e12d4e79e32327f
pedro mendez <prmendez222@gmail.com>
2026-03-07 00:17:42 -0600

    ejercicio 10 completado

---
+++ ejercicio 10/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Timers;
+
+void Presionar()
+{
+    Console.WriteLine();
+    Console.WriteLine("Presione Enter Para continuar");
+    Console.ReadLine();
+}
+Dictionary<int, string> prodructos = new Dictionary<int, string>();
+string opcion;
+int contP = 0;
+do
+{
+    Console.WriteLine("1. ingresar Producto");
+    Console.WriteLine("2. ver lista de productos");
+    Console.WriteLine("3. eliminar Producto");
+    Console.WriteLine("4. salir");
+    Console.WriteLine();
+    Console.Write("ingrese una opcion: ");
+    opcion = Console.ReadLine();
+    Console.Clear();
+
+    switch (opcion)
+    {
+        case "1":
+            Console.Write("ingrese nombre del Producto: ");
+            prodructos.Add(contP, Console.ReadLine());
+            contP++;
+            Console.WriteLine();
+            Console.WriteLine("Producto ingresado con exito");
+            Presionar();
+            break;
+        case "2":
+            if (prodructos.Count == 0)
+            {
+                Console.WriteLine("no hay productos ingresados");
+            }
+            else
+            {
+                Console.WriteLine("ID         nombre del Producto");
+                foreach (var p in prodructos)
+                {
+                    Console.WriteLine($"{p.Key}          {p.Value}");
+                }
+            }
+            Presionar();
+            break;
+        case "3":
+            Console.Write("ingrese ID del producto: ");
+            int idP = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if (prodructos.ContainsKey(idP))
+            {
+                prodructos.Remove(idP);
+                Console.WriteLine("el producto a sido eliminado con exito");
+            }
+            else
+            {
+                Console.WriteLine("el producto no existe");
+            }
+            Presionar();
+            break;
+        case "4":
+            Console.WriteLine("saliendo...");
+            Thread.Sleep(2000);
+            break;
+        default:
+            Console.WriteLine("opcion no valida");
+            Presionar();
+            break;
+    }
+
+    Console.Clear();
+
+} while (opcion != "4");
